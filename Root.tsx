e2a83a0ed89e188bf8eb20663b41207a88a99128
sgoycoechea <santiago.goycoechea@lightit.io>
2023-05-11 10:13:36 -0300

    feat: Sign in

---
+++ Root.tsx
@@ -0,0 +1,78 @@
+import 'react-native-gesture-handler';
+import React, { useContext, useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import { AuthContext, AuthContextType } from './context/AuthProvider';
+import LoginScreen from './screens/Auth/LoginScreen';
+import * as SecureStore from 'expo-secure-store';
+
+const Stack = createStackNavigator();
+
+const HomeStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home Screen"
+        component={HomeScreen}
+        options={{ title: 'Home' }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const AuthStackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false, headerBackTitleVisible: false }}
+    >
+      <Stack.Screen
+        name="Login Screen"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { user, setUser } = useContext<AuthContextType>(AuthContext);
+
+  useEffect(() => {
+    SecureStore.getItemAsync('user')
+      .then(userString => {
+        if (userString) {
+          setUser(JSON.parse(userString));
+        }
+        setIsLoading(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+   }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator size="large" color="gray" />
+      </View>
+    );
+  }
+
+  return (
+    <>
+      {user ? (
+        <NavigationContainer>
+        <HomeStackNavigator />
+      </NavigationContainer>
+      ) : (
+        <NavigationContainer>
+          <AuthStackNavigator />
+        </NavigationContainer>
+      )}
+    </>
+  );
+}
